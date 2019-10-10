8ab8aaeddf6d0708a1a5478e209167d37d2bb3ae
Radu Mogos <radu.mogos@neho.ch>
2019-10-10 21:19:22 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.base')
+
+@section('content')
+    <div class="row">
+        <div class="col-6">
+            Left
+        </div>
+        <div class="col-6">
+            Right
+        </div>
+    </div>
+@endsection
